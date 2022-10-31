9554e509ad8f1994107aae87384f2fe74f678af8
Martin Vladic <martin.vladic@gmail.com>
2022-10-31 14:42:11 +0100

    lvgl build redesign

--- src/ui/flow_def.h
+++ src/ui/flow_def.h
@@ -1,23 +1,8 @@
-#pragma once
+#ifndef EEZ_LVGL_UI_FLOW_DEF_H
+#define EEZ_LVGL_UI_FLOW_DEF_H
 
-#include <eez/gui/data.h>
-#include <eez/gui/widget.h>
-
-#include "lvgl/lvgl.h"
-
-namespace eez {
-
-enum ThemesEnum {
-    THEME_ID_DEFAULT = 0
-};
-
-enum ColorsEnum {
-    COLOR_ID_TRANSPARENT = 65535,
-    COLOR_ID_COLOR = 0,
-    COLOR_ID_BACKGROUND_COLOR = 1,
-	COLOR_ID_CUSTOM_UNDEFINED = 2
-};
+#include <stdint.h>
 
 extern const uint8_t assets[508];
 
-} // namespace eez
+#endif /*EEZ_LVGL_UI_FLOW_DEF_H*/
